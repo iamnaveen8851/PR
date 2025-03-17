ab651cfd4cdbebe2de8d0d77f60401a8a511c684
iamnaveen8851 <nk24528.nk81@gmail.com>
2025-03-17 19:06:27 +0530

    signup instance commented for testing

--- frontend/src/redux/actions/signUpAction.js
+++ frontend/src/redux/actions/signUpAction.js
@@ -10,15 +10,15 @@ const signUpData = (data) => {
 export const handleSignUp = (formState, navigate) => {
   return async (dispatch) => {
     try {
-      // const res = await axios.post(
-      //   "http://localhost:8080/users/signup",
-      //   formState
-      // );
-
-      const res = await axiosInstance.post(
-        import.meta.env.VITE_SIGNUP,
+      const res = await axios.post(
+        "http://localhost:8080/users/signup",
         formState
       );
+
+      // const res = await axiosInstance.post(
+      //   import.meta.env.VITE_SIGNUP,
+      //   formState
+      // );
       if (res.status === 201) {
         dispatch(signUpData(res.data.message));
         navigate("/");
